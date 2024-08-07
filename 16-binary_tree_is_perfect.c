19998a46ce9b5c61aea0793546e35fff3da484db
Pandor3 <thatdumbphilosopher@gmail.com>
2024-08-07 09:32:18 +0000

    Started testing on 16th exercise

---
+++ 16-binary_tree_is_perfect.c
@@ -0,0 +1,14 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_is_perfect - function which checks if a binary -
+ * - tree is perfect or not
+ *   @tree: pointer to the root node
+ *   Return: 0 or i
+ */
+
+int binary_tree_is_perfect(const binary_tree_t *tree)
+{
+	int count = 0;
+
+}
